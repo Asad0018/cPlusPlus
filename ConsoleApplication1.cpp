9731949b90de48330e0dae231ec1ff9092af1200
Asad0018 <dangerasad259@gmail.com>
2024-12-23 01:46:04 +0330

    Update ConsoleApplication1.cpp

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -13,13 +13,12 @@ int GetInt()
 // Inputs an array of doubles:
 vector<double> GetDoubles()
 {
-    int size;
     cout << "How many numbers do you wanna enter? ";
-    cin >> size;
+    int size; cin >> size;
     vector<double> arr(size);
 
     cout << "Enter " << size << " numbers:\n";
-    for (double i = 0; i < size; ++i)
+    for (int i = 0; i < size; ++i)
         cin >> arr[i];
     return arr;
 }
